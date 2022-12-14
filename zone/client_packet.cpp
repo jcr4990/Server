ef1eeecbe5a73261e17aae2e2cd503900c143d40
Akkadius <akkadius1@gmail.com>
2022-12-14 03:57:21 -0600

    Add charges to ITEM_DESTROY

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -5394,6 +5394,7 @@ void Client::Handle_OP_DeleteItem(const EQApplicationPacket *app)
 		auto e = PlayerEvent::DestroyItemEvent{
 			.item_id = inst->GetItem()->ID,
 			.item_name = inst->GetItem()->Name,
+			.charges = inst->GetCharges(),
 			.reason = "Client deleted",
 		};
 
