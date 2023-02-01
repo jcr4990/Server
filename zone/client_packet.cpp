90498890a238cbe11a24616af25f2491403b5739
Akkadius <akkadius1@gmail.com>
2023-02-01 04:42:28 -0600

    Replace very old eventlog table since the same events are covered by player event logs

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -2235,9 +2235,6 @@ void Client::Handle_OP_AdventureMerchantSell(const EQApplicationPacket *app)
 		return;
 	}
 
-	if (RuleB(EventLog, RecordSellToMerchant))
-		LogMerchant(this, vendor, ams_in->charges, price, item, false);
-
 	if (!inst->IsStackable())
 	{
 		DeleteItemInInventory(ams_in->slot);
@@ -13422,9 +13419,6 @@ void Client::Handle_OP_ShopPlayerBuy(const EQApplicationPacket *app)
 	}
 	// end QS code
 
-	if (RuleB(EventLog, RecordBuyFromMerchant))
-		LogMerchant(this, tmp, mpo->quantity, mpo->price, item, true);
-
 	const auto& export_string = fmt::format(
 		"{} {} {} {} {}",
 		tmp->GetNPCTypeID(),
@@ -13536,9 +13530,6 @@ void Client::Handle_OP_ShopPlayerSell(const EQApplicationPacket *app)
 	}
 	else
 		mp->quantity = 1;
-
-	if (RuleB(EventLog, RecordSellToMerchant))
-		LogMerchant(this, vendor, mp->quantity, price, item, false);
 
 	int charges = mp->quantity;
 
@@ -14518,10 +14509,6 @@ void Client::Handle_OP_TradeAcceptClick(const EQApplicationPacket *app)
 				// TODO: query (other) as a hacker
 			}
 			else {
-				// Audit trade to database for both trade streams
-				other->trade->LogTrade();
-				trade->LogTrade();
-
 				other->PlayerTradeEventLog(other->trade, trade);
 
 				// start QS code
